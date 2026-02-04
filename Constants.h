e82c47d69b664374fa1fb8bcd06e41d006844612
daylyn-n <daylynnguyen33@gmail.com>
2026-02-04 12:39:49 -0800

    ok

--- Constants.h
+++ Constants.h
@@ -2,13 +2,13 @@
 #define CONSTANTS_H
  
 #define WIDTH 1000
-#define HEIGHT 1000
-#define MAX_PARTICLE 100
+#define HEIGHT 800
+#define MAX_PARTICLE 30000
 #define GRAVITY 9.8
 #define FRICTION 0.1
 
 // how elsatic our particles are
-#define gCOR 0.8
+#define gCOR 0.9
 enum RUNNING
 {
     IS_RUNNING, NOT_RUNNING
